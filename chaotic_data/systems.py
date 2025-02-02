7541d797ed97d78483838b794c262926a1975337
rock-feller <rockefeller.r@moraeglobal.com>
2025-02-02 22:29:54 +0200

    Added the implementation of the three fully obs  chaotic systems

---
+++ chaotic_data/systems.py
@@ -0,0 +1,203 @@
+import numpy as np
+from scipy.integrate import odeint
+from typing import Tuple
+import torch
+
+
+
+def lorenz(state , t):
+    s=10
+    r=28
+    b=2.667
+    x, y, z = state
+    """
+    Inputs:
+       x, y, z: a point of interest in three dimensional space
+       s, r, b: parameters defining the lorenz attractor
+    
+    Then:
+    Put together the lorenz equations in the form:
+
+    x_dot : dx/dt = s(y - x)
+    y_dot : dy/dt = rx - y - xz
+    z_dot : dz/dt = xy - bz
+
+    Outputs:
+            - x_dot
+            - y_dot
+            - z_dot
+    values of the lorenz attractor's partial derivatives at the point x, y, z
+    """
+    x_dot = s*(y - x)
+    y_dot = r*x - y - x*z
+    z_dot = x*y - b*z
+
+    return x_dot, y_dot, z_dot
+  
+def LorenzSolver(start :int, stop:int, ics : Tuple[float , float , float] ,
+                 time_grid : float)-> Tuple[np.ndarray , np.ndarray , np.ndarray, np.ndarray]:
+    
+    """
+    Inputs:
+            -  start: the start time of the lorenz system : int
+            -  stop: the stop time of the lorenz system : int
+            - ics: initial conditions of the lorenz system 
+            - time grid : the time grid of the lorenz system :
+    [start and stop] correspond to the the range within which you want the data to be generated
+    The larger the time grid , the less spaced the data points will be.
+    The nber of points = (stop - start) / time_grid
+    
+    Then, 
+    Solve the Lorenz system of equations for the given initial conditions and time range
+
+    Outputs:
+            - x values of the lorenz system
+            - y values of the lorenz system
+            - z values of the lorenz system
+            - time values of the lorenz system
+    
+    Outputs shape: Tensor shape : (nber of points , 3, 1)
+    """
+    x0 , y0 , z0 = ics
+    
+    state0 = [x0 , y0 , z0]
+    timegrid_array = np.arange(start, stop, time_grid)
+    states = odeint(lorenz, state0, timegrid_array)
+    xs = states[:,0]
+    ys = states[:,1]
+    zs = states[:,2]
+
+    lor_trajs= torch.tensor(np.vstack([xs,ys,zs]).T , dtype= torch.float32)
+
+    return lor_trajs, timegrid_array
+
+
+
+
+
+def rossler_eqs(state, t):
+    a = 0.2
+    b = 0.2
+    c = 5.7
+    x, y, z = state
+    """
+    Inputs:
+       x, y, z: a point of interest in three dimensional space
+       a, b, c: parameters defining the Rössler attractor
+    
+    Then:
+    Put together the Rössler equations in the form:
+
+    x_dot : dx/dt = -y - z
+    y_dot : dy/dt = x + ay
+    z_dot : dz/dt = b + z(x - c)
+
+    Outputs:
+            - x_dot
+            - y_dot
+            - z_dot
+    values of the Rössler attractor's partial derivatives at the point x, y, z
+    """
+    x_dot = -y - z
+    y_dot = x + a * y
+    z_dot = b + z * (x - c)
+
+    return x_dot, y_dot, z_dot
+
+def RosslerSolver(start: int, stop: int, ics: Tuple[float, float, float], time_grid: float) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    
+    """
+    Inputs:
+            - start: the start time of the Rössler system : int
+            - stop: the stop time of the Rössler system : int
+            - ics: initial conditions of the Rössler system 
+    [start and stop] correspond to the the range within which you want the data to be generated
+    
+    Then, 
+    Solve the Rössler system of equations for the given initial conditions and time range
+
+    Outputs:
+            - x values of the Rössler system
+            - y values of the Rössler system
+            - z values of the Rössler system
+            - time values of the Rössler system
+
+     Outputs shape: Tensor shape : (nber of points , 3, 1)
+
+    """
+    x0, y0, z0 = ics
+    state0 = [x0, y0, z0]
+    state0 = [x0 , y0 , z0]
+    timegrid_array = np.arange(start, stop, time_grid)
+    states = odeint(rossler_eqs, state0, timegrid_array)
+    xs = states[:,0]
+    ys = states[:,1]
+    zs = states[:,2]
+
+    ross_trajs= torch.tensor(np.vstack([xs,ys,zs]).T , dtype= torch.float32)
+
+    return ross_trajs, timegrid_array
+
+
+def rabi_fabri_eqs(state, t):
+    alpha = 0.14
+    gamma = 0.1
+    x, y, z = state
+    """
+    Inputs:
+       x, y, z: a point of interest in three dimensional space
+       alpha, gamma: parameters defining the Rabinovich-Fabrikant attractor
+    
+    Then:
+    Put together the Rabinovich-Fabrikant equations in the form:
+
+    x_dot : dx/dt = y(z - 1 + x^2) + gamma * x
+    y_dot : dy/dt = x(3z + 1 - x^2) + gamma * y
+    z_dot : dz/dt = -2z(alpha + xy)
+
+    Outputs:
+            - x_dot
+            - y_dot
+            - z_dot
+    values of the Rabinovich-Fabrikant attractor's partial derivatives at the point x, y, z
+
+   
+    """
+    x_dot = y * (z - 1 + x**2) + gamma * x
+    y_dot = x * (3 * z + 1 - x**2) + gamma * y
+    z_dot = -2 * z * (alpha + x * y)
+
+    return x_dot, y_dot, z_dot
+
+def RabinovichFabrikantSolver(start: int, stop: int, ics: Tuple[float, float, float], 
+                              time_grid: float) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    
+    """
+    Inputs:
+            - start: the start time of the Rabinovich-Fabrikant system : int
+            - stop: the stop time of the Rabinovich-Fabrikant system : int
+            - ics: initial conditions of the Rabinovich-Fabrikant system 
+    [start and stop] correspond to the the range within which you want the data to be generated
+    
+    Then, 
+    Solve the Rabinovich-Fabrikant system of equations for the given initial conditions and time range
+
+    Outputs:
+            - x values of the Rabinovich-Fabrikant system
+            - y values of the Rabinovich-Fabrikant system
+            - z values of the Rabinovich-Fabrikant system
+            - time values of the Rabinovich-Fabrikant system
+            
+     Outputs shape: Tensor shape : (nber of points , 3, 1)
+    """
+    x0, y0, z0 = ics
+    state0 = [x0, y0, z0]
+    timegrid_array = np.arange(start, stop, time_grid)
+    states = odeint(rabi_fabri_eqs, state0, timegrid_array)
+    xs = states[:,0]
+    ys = states[:,1]
+    zs = states[:,2]
+
+    rabifabri_trajs= torch.tensor(np.vstack([xs,ys,zs]).T , dtype= torch.float32)
+
+    return rabifabri_trajs, timegrid_array
---
+++ chaotic_data/utils.py
@@ -0,0 +1,301 @@
+import torch
+import numpy as np
+import random
+from typing import Tuple , Literal
+from torch.utils.data import Dataset, DataLoader
+
+
+
+
+class Normalization_Strategy():
+
+    def __init__(self):
+
+        self.data_normalization = True
+
+
+    def Scale_ToNormalize(self, data :  torch.Tensor, 
+                            scaling_factor: float) -> Tuple[np.ndarray , np.ndarray]:
+        """
+        Inputs:
+                - the tensor data of shape (samples,features ) 
+                - a scaling factor (float)
+        
+        Then, 
+        Normalize the data  around 0 and multiply by the scaling factor according to the formula,
+        scaled_data = scaling_factor*(data - data_averages)
+        
+        Outputs: 
+                - the scaled data of shape  (samples,features,1) 
+                - the average of the tensor data of shape (features,1)
+        
+                
+        Inputs Shape: Tensor of shape (samples,features , 1) , float
+        Outputs Shape: Tensor of shape(samples,features,1) , Tensor of shape (features,1)
+        
+        """
+
+        tens_avgs = data.mean(axis=0)
+        scaled_tens_data = scaling_factor*(data - tens_avgs) 
+
+        
+        return scaled_tens_data , tens_avgs
+
+    def ScaleBackTo_originals(self, scaled_tens_data : torch.Tensor, scaling_factor: float ,
+                           tens_avgs: torch.Tensor) ->  torch.Tensor:
+
+        """This function takes as
+        Inputs:
+                - the scaled data of shape (samples,features,1) 
+                - a scaling factor (float)
+                - the average of the tensor data of shape (features,1)
+
+        Then, 
+        multiply the scaled data by the inverse of the scaling factor and
+        add the averages along each axis
+        
+        
+        Outputs: 
+                - the original numpy array data of shape (samples,features)
+                
+        
+        Inputs Shape: Tensor of shape (samples,features , 1) , float , Tensor of shape (features,1)
+        Outputs Shape: Array of shape(samples,features,1) , Tensor of shape (samples , features)        
+                
+        """
+
+        original_tens_data =  (scaled_tens_data / scaling_factor) + tens_avgs
+        
+        original_array_data  = original_tens_data.detach().cpu().numpy().squeeze()
+        return original_array_data 
+
+
+
+
+class Noisification_Strategy():
+    def __init__(self ):
+        self.add_noise = True
+
+
+    def adding_sigmanoise_from_normal(self, chaos_trajs : torch.Tensor , sigma: float) -> torch.Tensor:
+        """
+        Inputs Shape: Tensor of shape (samples,features , 1) 
+        Outputs Shape: Tensor of shape (samples,features , 1) 
+        
+        """
+        noise_x =  np.random.normal(0,sigma,len(chaos_trajs))
+        noise_y =  np.random.normal(0,sigma,len(chaos_trajs))
+        noise_z =  np.random.normal(0,sigma,len(chaos_trajs))
+
+        noisy_trajs = chaos_trajs + torch.tensor(np.vstack([noise_x,noise_y,noise_z]).T, 
+                                                dtype= torch.float32).unsqueeze(2)
+
+        return noisy_trajs
+    
+
+    def other_noise_strategy(self, chaos_trajs : torch.Tensor , sigma: float) -> torch.Tensor:
+        return None
+    
+
+
+
+
+
+class SlidingWindowDataset(Dataset):
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+    def __init__(self, tensor_data: torch.tensor , window_size:int):
+        """
+        Initializes the dataset with sliding windows.
+        
+        Inputs shape:
+          - tensor_data: tensor of shape (sam[ples , features)
+          - window_size: number of time steps to look back
+  
+        """
+        self.tensor_data = tensor_data
+        self.window_size = window_size
+        self.slided_data = self.sliding_windows_shift_to(tensor_data, window_size)
+
+    def sliding_windows_shift_to(self, tensor_data, window_size)-> torch.Tensor :#, tensor_data, window_size) -> torch.Tensor:
+        """
+        Creates sliding window from data.
+        
+        Args:
+        data (torch.tensor): Input tensor of shape (m, 1, p)
+        window_size (int): The size of the sliding window.
+        
+        Returns:
+        torch.tensor: 4D tensor of shape (num_batches, window_size, 1, p)
+        """
+        num_batches = len(tensor_data) - window_size + 1
+        x = []
+        for i in range(num_batches):
+            _x = tensor_data[i:i+window_size]
+            x.append(_x)
+        return torch.stack(x).float().to(self.device)  # Ensures that the output is double precision
+
+    def __len__(self):
+        return len(self.slided_data)
+
+    def __getitem__(self, idx):
+        return self.slided_data[idx]
+    
+
+
+    def contextwindow_testdata_generator(self, context_size :int, fcast_size:int,  location: Literal["random_", "last_"]):
+
+        if location.lower() == 'random_':
+            """
+             Inputs Shape:
+              -  context_size : int
+             -  fcast_size: int 
+              - location : str : random_  or last_ 
+              
+              Outputs Shape:
+
+               - context_window: tensor of shape (context_size, features)
+               - windowdata_toforecast: tensor of shape (fcast_size, features)
+              """
+
+            full_data = self.sliding_windows_shift_to(self.tensor_data, context_size +fcast_size )
+            random_window_id =  random.randint( 0, full_data.size(0))
+            context_window   = full_data[random_window_id,:context_size].squeeze(1)
+            windowdata_toforecast = full_data[random_window_id,context_size:].squeeze(1)
+            
+            return context_window  , windowdata_toforecast
+
+        elif  location.lower() == 'last_':
+            """Here you must only pass in the entire dataset as a tensor of shape [n_samples, n_features]"""
+            full_data = self.sliding_windows_shift_to(self.tensor_data, context_size +fcast_size )
+            context_window   = full_data[-1,:context_size].squeeze(1)
+            windowdata_toforecast = full_data[-1,context_size:].squeeze(1)
+            return context_window  , windowdata_toforecast
+
+        else:
+            raise ValueError("s0_nature must be either 'random_' or 'last_' ")
+        
+
+class SlidingWindowDataLoader(DataLoader):
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+    #dtype = torch.float32
+    def __init__(self, dataset, batch_size=32, shuffle=False, device=device):
+        """
+        Custom DataLoader that loads batches from a dataset.
+        
+        Args:
+        dataset (SlidingWindowDataset): The dataset to load from.
+        batch_size (int): Number of samples per batch.
+        shuffle (bool): Whether to shuffle the data every epoch.
+        device (torch.device): The device to map the data to.
+        """
+        super().__init__(dataset, batch_size=batch_size, shuffle=shuffle )
+        #self.device = device
+
+    def __iter__(self):
+        """
+        Iterate over batches and map data to the specified device.
+        """
+        batches = super().__iter__()
+        for batch in batches:
+            yield batch.to(self.device)
+
+
+
+
+class InpTarg_TSDataset(Dataset):
+    def __init__(self, data: torch.Tensor, window_size: int):
+        self.data = data
+        self.window_size = window_size
+
+    def __len__(self):
+        return len(self.data) - self.window_size
+
+    def __getitem__(self, idx: int):
+        input_seq = self.data[idx:idx+self.window_size]
+        target_seq = self.data[idx+1:idx+self.window_size+1]
+        return input_seq, target_seq
+
+    @classmethod
+    def create_train_loader(cls, data: torch.Tensor, window_size: int, batch_size: int, shuffle: bool = False) -> DataLoader:
+        """
+        Inputs shape: 
+            - tensor data of shaope (samples, features)
+            - window_size: number of time steps to look back
+            - batch_size: number of samples per batch
+            - shuffle: whether to shuffle the data
+        Outputs:
+            - train_loader: DataLoader object : iterables over the dataset with inp_ and out_ batches of shape (batch_size, window_size, features)
+        
+        """
+        train_dataset = cls(data, window_size)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+        return train_loader
+    
+
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+import random
+
+class SlidingWindowDataset(Dataset):
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+    def __init__(self, data, window_size):
+        """
+        Initializes the dataset with sliding windows.
+        
+        Args:
+        data (torch.tensor): Input tensor of shape (m, 1, p)
+        window_size (int): The size of the sliding window.
+        """
+        self.data = data
+        self.window_size = window_size
+        self.slided_data = self.sliding_windows_shift_to(data, window_size)
+
+    def sliding_windows_shift_to(self, data, window_size) -> torch.Tensor:
+        """
+        Creates sliding window from data.
+        
+        Args:
+        data (torch.tensor): Input tensor of shape (m, 1, p)
+        window_size (int): The size of the sliding window.
+        
+        Returns:
+        torch.tensor: 4D tensor of shape (num_batches, window_size, 1, p)
+        """
+        num_batches = len(data) - window_size + 1
+        x = []
+        for i in range(num_batches):
+            _x = data[i:i+window_size]
+            x.append(_x)
+        return torch.stack(x).float().to(self.device)  # Ensures that the output is double precision
+
+    def __len__(self):
+        return len(self.slided_data)
+
+    def __getitem__(self, idx):
+        return self.slided_data[idx]
+    
+
+
+    def contextwindow_testdata_generator(self, context_size :int, fcast_size:int,  location: Literal["random_", 'last_']):
+
+        if location.lower() == 'random_':
+            """Here you must only pass in the train dataset  as a tensor of shape [n_samples, n_features]"""
+
+            full_data = self.sliding_windows_shift_to(self.data, context_size +fcast_size )
+            random_window_id =  random.randint( 0, full_data.size(0))
+            context_window   = full_data[random_window_id,:context_size].squeeze(1)
+            windowdata_toforecast = full_data[random_window_id,context_size:].squeeze(1)
+            
+            return context_window  , windowdata_toforecast
+
+        elif  location.lower() == 'last_':
+            """Here you must only pass in the entire dataset as a tensor of shape [n_samples, n_features]"""
+            full_data = self.sliding_windows_shift_to(self.data, context_size +fcast_size )
+            context_window   = full_data[-1,:context_size].squeeze(1)
+            windowdata_toforecast = full_data[-1,context_size:].squeeze(1)
+            return context_window  , windowdata_toforecast
+
+        else:
+            raise ValueError("s0_nature must be either 'random_' or 'last_' ")
