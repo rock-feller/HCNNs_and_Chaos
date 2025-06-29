5d879d0f3afdba596a66bf4414e43ef9b8b1bfa0
rock-feller <rockefeller.r@moraeglobal.com>
2025-06-29 22:04:28 +0200

    Enforced variable type in logcosh class definition

--- src/model_utils/custom_losses.py
+++ src/model_utils/custom_losses.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 
 
 class LogCoshLoss(nn.Module):
-    def __init__(self, p=0.02):
+    def __init__(self, p:float=0.02):
         super(LogCoshLoss, self).__init__()
         self.p = p
 
-    def forward(self, y_pred, y_true):
+    def forward(self, y_pred:torch.Tensor, y_true:torch.Tensor) -> float:
         error = y_pred - y_true
         log_cosh_error = torch.log(torch.cosh(error))
         loss = torch.mean(log_cosh_error) *self.p
